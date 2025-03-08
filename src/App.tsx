710785a84c393220f76a4eec7e94e09ad41a725c
Swapnil <swapnillandage79@gmail.com>
2025-03-08 18:31:43 +0530

    create initial route setups

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,7 @@ import { ErrorBoundary } from './components/ErrorBoundary';
 const App: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      {/* Top Navigation */}
       <Navbar />
-      {/* Main Layout: Sidebar + Content */}
       <div className="flex flex-1">
         <Sidebar />
         <main className="flex-1 p-6 overflow-auto">
